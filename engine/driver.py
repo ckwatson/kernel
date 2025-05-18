4a063c61d552b5a05f2b2ef14df8e144133e7c06
Mingyang Li <tslmy@outlook.com>
2025-05-18 01:02:21 -0700

    Refactored `preequilibrate_reagent`.

--- engine/driver.py
+++ engine/driver.py
@@ -4,18 +4,17 @@
 """Solve the equilibrium ode"""
 
 # These modules are used to parse arguments
-import sys
 import os
 import logging
 from typing import Optional
-
+from tabulate import tabulate
 # These are the programs modules
 # from . import input_output
-from . import fileIO, experiment_class
+from . import experiment_class
 from . import handy_functions as HANDY
 import numpy as np
 
-from ..data import solution_class, condition_class
+from ..data import solution_class, condition_class, reaction_mechanism_class
 
 diag = os.path.join("Diagnose" + os.sep)
 graph = os.path.join("Graphs" + os.sep)
@@ -26,7 +25,7 @@ graph = os.path.join("Graphs" + os.sep)
 # this function executes the necessary mathematical operations to find the Keq array - i.e. equilibrates the reaction
 
 
-def equilibrate(input_model, in_conc=None, diag=False):
+def equilibrate(input_model: experiment_class.experiment, diag=False):
     # the meat and potatoes
     input_model.find_rate_constant()
     input_model.find_reaction_rate_function()
@@ -36,87 +35,11 @@ def equilibrate(input_model, in_conc=None, diag=False):
     return True
 
 
-def show_all_concentrations(job_id: str, condition):
-    logger = logging.getLogger(job_id).getChild("show_all_concentrations")
-    logger.info(
-        "                    Update the concentration record of the whole lab:")
-    buff = '                        '
-    for i in condition.molecule_concentrations.keys():
-        buff += i + '\t'
-    buff = '                        '
-    for i in condition.molecule_concentrations.values():
-        buff += str(i)[:4] + "\t"
-    logger.info(buff)
-
-
 def run_true_experiment(job_id: str, puzzle, condition) -> np.ndarray:
     logger = logging.getLogger(job_id).getChild("run_true_experiment")
     logger.info("            First, pre-equilibrate every reagent:")
-    # zero out all the condition objects molecule concentrations as a safety measure
-    # this should probably be made a funciton in the condition class at some later point
-    for reagent_name, reagent_obj in puzzle.reagent_dict.items():
-        logger.info("                Pre-equilibrate " + reagent_name + ":")
-        for name, index in reagent_obj.coefficient_dict.items():
-            condition.molecule_concentrations[name] = 0
-
-        # make the array that will hold the concentration values for each REAGENT, the "beakers" that the user can change the conc of
-        reagent_conc_array = np.zeros((1, reagent_obj.number_of_species))
-        # we start with the concentrations defined by the user in the condition object they passed to us
-        reagent_conc_array[0][reagent_obj.coefficient_dict[reagent_name]
-        ] = condition.reagent_concentrations[reagent_name]
-        #
-        #logger.info("Reagent temps: " + str(condition.reagent_temperatures))
-
-        # suppose the REAGENT is O2, then by itself in a canister or beaker it would actually be comprised
-        # of some small amount of O and O2, as the O2 can dissociate.
-        # therefore we may need to pre-equilibrate
-
-        # if there is only one species no pre-equilibration is needed as no dissociation can occur
-        if reagent_obj.number_of_species == 1:
-            # then directly add the reagents concentrations to their associated molecule concentrations
-            condition.molecule_concentrations[name] += condition.reagent_concentrations[reagent_name]
-
-            logger.info(
-                "                    This reagent only has one species so no pre-equilibration happened.")
-            logger.info("                    This reagent had a concentration of " +
-                        str(condition.reagent_concentrations[reagent_name]))
-            show_all_concentrations(job_id, condition)
-            # this break is for the loop on line 44, remember that we check each reagent for pre-equilibration
-            # some reagents may need to pre-equilibrate and some may not
-            break
-
-        # create the experiment object, this object handles all the necessary mathematical calculations to determine
-        # how much of each species is created, consumed during dissociation/pre-equilibration
-        pre_equil_model = experiment_class.experiment(reagent_obj,
-                                                      condition.reagent_temperatures[reagent_name],
-                                                      rxn_profile=reagent_conc_array
-
-                                                      )
-
-        # actually preform the mathematical calculations
-        # (diagnostics) diag is an optional argument that if true prints all the output from the integrator inside the experiment object
-        # this is a lotttt of extra output
-        logger.info("			* reagent_obj.coefficient_dict: " +
-                    str(reagent_obj.coefficient_dict))
-        logger.info("			* reagent_conc_array -- before: " +
-                    str(reagent_conc_array))
-
-        equilibrate(pre_equil_model, diag=False)
-        logger.info("			* reagent_conc_array -- after: " +
-                    str(pre_equil_model.reaction_profile[-1]))
-        # finally after we have obtained the new equilibrated concentrations we place those in the condition object
-        for name, index in reagent_obj.coefficient_dict.items():
-            concentration_of_this_species = pre_equil_model.reaction_profile[-1][index]
-            if np.isnan(concentration_of_this_species):
-                logger.info('                    [Warning] Concentration of ' + name +
-                            ' is NaN. Falling back to un-pre-equilibrated concentrations. This is a bug.')
-                concentration_of_this_species = reagent_conc_array[0][index]
-            condition.molecule_concentrations[name] += concentration_of_this_species
-
-        # collect the equilibrated concentrations
-        show_all_concentrations(job_id, condition)
-        # we no longer need this obj
-        del pre_equil_model
+    for reagent_name, mechanism in puzzle.reagent_dict.items():
+        preequilibrate_reagent(job_id, mechanism, condition, reagent_name)
 
     # now that we have left the loop over REAGENTS we have pre-equilibrated all necessary REAGENTS
     logger.info(
@@ -134,8 +57,7 @@ def run_true_experiment(job_id: str, puzzle, condition) -> np.ndarray:
     # now we perform the same mathematical operations as before, but this time we have all the molecules present instead of isolated reactions
     logger.info(
         "            Now we can finally let the actual reaction happen -- let's pour everything into the beaker:")
-    equilibrate(true_model, in_conc=condition.reagent_concentrations,
-                diag=False)  # the magical math happens
+    equilibrate(true_model, diag=False)  # the magical math happens
 
     # for the progress bar, to represent that we finished calculating the true model
 
@@ -158,10 +80,101 @@ def run_true_experiment(job_id: str, puzzle, condition) -> np.ndarray:
     return written_data
 
 
+def preequilibrate_reagent(job_id: str, mechanism: reaction_mechanism_class.reaction_mechanism, condition: condition_class.Condition, reagent_name: str):
+    """
+    Some reagents may dissociate into multiple species even when sitting ideally in a canister/beaker, so we need to
+    pre-equilibrate them.
+
+    For example, a canister/beaker that was filled with pure O2 would actually be comprised of some small amount
+    of O and O2, as the O2 can dissociate on its own.
+
+    This does not happen for all reagents, though. For example, H2 does not dissociate into H and H2.
+
+    How do we know which reagents need to be pre-equilibrated? It's defined in the puzzle definition file.
+    The field `reagentPERs` tells us, for each reagent in the puzzle, and for each elementary reaction in the puzzle,
+    whether the reaction is would naturally happen in a canister/beaker of that reagent.
+
+    For example, if the puzzle has 5 elementary reactions, and the reagent is C2H6, then this snippet:
+
+    ```json
+        "reagentPERs": {
+        "C2H6": [
+            true,
+            false,
+            false,
+            false,
+            false
+        ]
+    }
+    ```
+
+    says that the first elementary reaction would happen in a canister/beaker of C2H6, but the other 4 would not.
+    """
+    logger = logging.getLogger(job_id).getChild("preequilibrate_reagent")
+    logger.info("                Pre-equilibrate " + reagent_name + ":")
+
+    # Zero out all the condition objects molecule concentrations as a safety measure.
+    # This should probably be made a function in the condition class at some later point.
+    for name in mechanism.coefficient_dict.keys():
+        condition.molecule_concentrations[name] = 0
+
+    reagent_id = mechanism.coefficient_dict[reagent_name]
+    user_specified_concentration = condition.reagent_concentrations[reagent_name]
+
+    # If there is only one species no pre-equilibration is needed as no dissociation can occur.
+    if mechanism.number_of_species == 1:
+        logger.info(
+            "                        This reagent only has one species so no pre-equilibration happened. "
+            "This reagent will join the experiment with the user-specified concentration of %s mol.",
+            user_specified_concentration)
+        # then directly add the reagents concentrations to their associated molecule concentrations
+        condition.molecule_concentrations[reagent_name] += user_specified_concentration
+    else:
+        # Make the array that will hold the concentration values for each REAGENT, the "beakers" that the user can change
+        # the concentration of.
+        starting_concentrations = [0] * mechanism.number_of_species
+        # we start with the concentrations defined by the user in the condition object they passed to us
+        starting_concentrations[reagent_id] = user_specified_concentration
+
+        # create the experiment object, this object handles all the necessary mathematical calculations to determine
+        # how much of each species is created, consumed during dissociation/pre-equilibration
+        pre_equil_model = experiment_class.experiment(
+            mechanism,
+            condition.reagent_temperatures[reagent_name],
+            rxn_profile=np.array([starting_concentrations])
+        )
+
+        # actually preform the mathematical calculations
+        # (diagnostics) diag is an optional argument that if true prints all the output from the integrator inside the experiment object
+        # this is a lotttt of extra output
+        equilibrate(pre_equil_model, diag=False)
+        final_concentrations = pre_equil_model.reaction_profile[-1]
+
+        table = tabulate([starting_concentrations, final_concentrations],
+                         headers=sorted(mechanism.coefficient_dict.keys(), key=mechanism.coefficient_dict.get),
+                         floatfmt=".4g", tablefmt="github")
+        logger.info("                        Concentrations before and after pre-equilibration: \n%s", table)
+
+        # Now that we have the pre-equilibrated concentrations, place them into the condition object.
+        for name, index in mechanism.coefficient_dict.items():
+            concentration_of_this_species = final_concentrations[index]
+            if np.isnan(concentration_of_this_species):
+                logger.warning('                 Concentration of "%s" is NaN. Falling back to un-pre-equilibrated concentrations. This is a bug.', name)
+                concentration_of_this_species = starting_concentrations[index]
+            condition.molecule_concentrations[name] += concentration_of_this_species
+
+    # collect the equilibrated concentrations
+    table = tabulate([condition.molecule_concentrations.values()],
+                     headers=condition.molecule_concentrations.keys(),
+                     floatfmt=".4g", tablefmt="github")
+    logger.info("                    Updated concentrations of all reagents before the main experiment take place:\n%s", table)
+
+
 def run_proposed_experiment(job_id: str, condition: condition_class.Condition, solution: solution_class.solution, data=np.ndarray) -> Optional[np.ndarray]:
     logger = logging.getLogger(job_id).getChild("run_proposed_experiment")
 
     # make the experiment object
+
     proposed_model = experiment_class.experiment(
         solution, condition.reaction_temperature, input_time=data[0][:], rxn_profile=np.swapaxes(data[1::1][:], 0, 1))
 
