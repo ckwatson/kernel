34bbc4322c4ddc8e164680b44794e8a7d87f2f10
Mingyang Li <tslmy@outlook.com>
2025-05-24 15:54:47 -0700

    fix test_reagent.py

--- tests/test_reagent.py
+++ tests/test_reagent.py
@@ -1,16 +1,35 @@
-from ..data.molecular_species import molecular_species
-from ..data.reagent           import reagent
 import numpy as np
-import pytest
+from deepdiff import DeepDiff
 
-def test_load_store():
-    test_coef = np.array([  [1.0, 1.0, -1.0],[-1.0, 2.0, -1.0]], dtype=float)
-    test_molecule_one   = molecular_species('H',  10.0, {'H' : 1, 'O' : 0})
-    test_molecule_two   = molecular_species('H2', 0.0,  {'H' : 2, 'O' : 0})
-    test_molecule_three = molecular_species('H3', 30.0, {'H' : 3, 'O' : 0})
-    test_molecule_list = [test_molecule_one, test_molecule_two, test_molecule_three]
-    test_reagent = reagent('H3', 15.0, 2, 3, test_molecule_list, test_coef)
-    print(test_reagent.name + '.reagent')
-    test_reagent.store(test_reagent.name + '.reagent')
-    new_reagent = reagent.load_reagent(test_reagent.name + '.reagent')
-    assert(test_reagent == new_reagent)
+from ..data.molecular_species import molecular_species
+from ..data.reagent import reagent
+
+def test_init():
+    test_reagent = reagent(
+        'H3', 15.0,
+        # The parameters below are for the class `reaction_mechanism`.
+        num_rxn=2, num_species=3,
+        mol_list=["H", "H2", "H3"],
+        rxn_array=np.array([[1.0, 1.0, -1.0], [-1.0, 2.0, -1.0]], dtype=float),
+        reagEnrg={'H3': 30.0, 'H2': 0.0, 'H': 10.0}
+    )
+    got = test_reagent.__dict__.copy()
+    del got['JSON_Encoder']
+
+    want = {
+        'name': 'H3',
+        'concentration': 15.0,
+        'number_of_reactions': 2,
+        'number_of_species': 3,
+        'molecular_species_dict': {
+            # TODO: Are we sure we don't need atom_list?
+            'H': molecular_species('H', 10.0),#, {'H': 1, 'O': 0}),
+            'H2': molecular_species('H2', 0.0),#, {'H': 2, 'O': 0}),
+            'H3': molecular_species('H3', 30.0)#, {'H': 3, 'O': 0})
+        },
+        'coefficient_dict': {'H': 0, 'H2': 1, 'H3': 2},
+        'coefficient_array': np.array([[1., 1., -1.], [-1., 2., -1.]]),
+        'reactant_coefficient_array': np.array([[0., 0., 1.], [1., 0., 1.]]),
+        'product_coefficient_array': np.array([[1., 1., 0.], [0., 2., 0.]])
+    }
+    assert not DeepDiff(got, want, ignore_order=True)
